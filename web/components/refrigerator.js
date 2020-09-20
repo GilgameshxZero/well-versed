e4953fa1708f75ab34e5f53bace799eb9efbc8ca
ronaywang <rona.y.wang@gmail.com>
2020-09-19 22:55:26 -0400

    fetch

--- web/components/refrigerator.js
+++ web/components/refrigerator.js
@@ -1,4 +1,4 @@
-import { importWebElement } from "../js/fetch.js";
+import {fetch, importWebElement } from "../js/fetch.js";
 import "./word.js";
 // import "./coords-display.js";
 
@@ -12,6 +12,37 @@ importWebElement(`refrigerator`, class extends HTMLElement {
 			};
 			this.anchor = this.shadowRoot.querySelector(`div[name="anchor"]`);
 			// this.coordsDisplay = this.shadowRoot.querySelector(`well-versed-coords-display`);
+
+			const prom1 = fetch ({
+				url:`./assets/verbs.txt`,
+			}
+			);
+			const prom2 = fetch ({
+				url:`./assets/adjectives.txt`,
+			}
+			);
+			const prom3 = fetch ({
+				url:`./assets/nouns.txt`,
+			}
+			);
+			const prom4 = fetch ({
+				url:`./assets/adverbs.txt`,
+			}
+			);
+
+			Promise.all([prom1, prom2, prom3, prom4]).then(function (responses) {
+				i = 0 
+				while (i < 4) {
+					j = 0
+					while (j < 4) {
+						k = Math.floor(Math.random() * 10000)
+						responses[i].str(" ").pop().split(" ")[k]
+						j+=1
+					}
+					
+				i+=1
+				}
+			})
 
 			this.dragging = false;
 			const onPointerMove = (e) => {
